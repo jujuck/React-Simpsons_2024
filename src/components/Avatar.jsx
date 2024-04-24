4c101faad32b284d7b96912c7aafca4bcb7d952d
Julien RICHARD <julien.y.richard@gmail.com>
2024-04-24 14:47:22 +0200

    Ajout des props types sur Avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,14 +1,21 @@
+import PropTypes from "prop-types";
 import "./Avatar.css";
 
 function Avatar(props) {
   return (
     <div className="card">
-      <img src={props.image} alt="avatar" />
+      <img src={props.image.push("Bob")} alt="avatar" />
       <h2>
         {props.lastName} {props.firstName}
       </h2>
     </div>
   );
 }
+
+Avatar.propTypes = {
+  image: PropTypes.string.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+};
 
 export default Avatar;
