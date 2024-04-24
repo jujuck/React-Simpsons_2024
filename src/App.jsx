a3cb28b1f59425b40f01fa1b567ea3108a4064a7
Julien RICHARD <julien.y.richard@gmail.com>
2024-04-24 14:10:03 +0200

    Création du composant Avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Avatar from "./components/Avatar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Les Simpsons</h1>
+      <Avatar />
     </>
   );
 }
